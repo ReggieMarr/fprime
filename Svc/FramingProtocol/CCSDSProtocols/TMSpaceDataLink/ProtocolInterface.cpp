48bdf5038fcf0b847de0044d58c0d1616a75fa39
Reg Marr <reginald.marr@reactiondynamics.space>
2024-11-23 12:59:25 -0500

    Using templates to transfer data through ccsds servcies

--- Svc/FramingProtocol/CCSDSProtocols/TMSpaceDataLink/ProtocolInterface.cpp
+++ Svc/FramingProtocol/CCSDSProtocols/TMSpaceDataLink/ProtocolInterface.cpp
@@ -7,11 +7,15 @@
 #include <array>
 #include "FpConfig.h"
 #include "FpConfig.hpp"
+#include "Fw/Com/ComBuffer.hpp"
+#include "Fw/Com/ComPacket.hpp"
 #include "Fw/Logger/Logger.hpp"
 #include "Fw/Types/Assert.hpp"
 #include "Fw/Types/Serializable.hpp"
 #include "Svc/FramingProtocol/CCSDSProtocols/CCSDSProtocolDefs.hpp"
+#include "Svc/FramingProtocol/CCSDSProtocols/TMSpaceDataLink/Channels.hpp"
 #include "Svc/FramingProtocol/CCSDSProtocols/TMSpaceDataLink/ManagedParameters.hpp"
+#include "Svc/FramingProtocol/CCSDSProtocols/TMSpaceDataLink/Services.hpp"
 #include "Utils/Hash/Hash.hpp"
 #include "Utils/Types/CircularBuffer.hpp"
 #include "TransferFrame.hpp"
@@ -21,6 +25,11 @@
 
 namespace TMSpaceDataLink {
 
+ProtocolEntity::ProtocolEntity(ManagedParameters_t const &params) :
+    m_params(params)
+{
+}
+
 bool ProtocolEntity::UserComIn_handler(Fw::Buffer data, U32 context) {
     // Forward data to the appropriate Virtual Channel
     U8 mcId = (context >> 8) & 0xFF;
@@ -28,24 +37,31 @@ bool ProtocolEntity::UserComIn_handler(Fw::Buffer data, U32 context) {
     // TODO should have some sort of routing function here that takes the context
     // and gets the MCID and GVCID
     bool channelStatus = true;
+    VCAServiceParameters_t params;
+    // VirtualChannelParams_t vcParams = m_params.physicalParams.subChannels.at(0).subChannels.at(0);
+    // MCID_t mcid = {m_params.physicalParams.transferFrameVersion, 0};
+    // GVCID_t gvcid = {mcid, 0};
+    // VirtualChannelSender<VCAService> vc(vcParams, m_params.physicalParams.transferFrameSize, gvcid);
+    // Fw::ComBuffer com(data.getData(), data.getSize());
+    // vc.transfer(com);
 
     return channelStatus;
 }
 
 void ProtocolEntity::generateNextFrame() {
     // Generate frames for Virtual Channels
-    for (U8 mcId = 0; mcId < 8; mcId++) {
-        std::array<Fw::Buffer, 8> vcFrames;
-        for (U8 vcId = 0; vcId < 8; vcId++) {
-            Fw::Buffer sdu;
-            m_virtualChannels[mcId][vcId].propogate(sdu);
-            vcFrames[vcId] = sdu;
-        }
-        // Multiplex VC frames into Master Channel
-        Fw::Buffer mcFrame;
-        m_masterChannels[mcId].VirtualChannelMultiplexing_handler(vcFrames);
-        m_masterChannels[mcId].MasterChannelGeneration_handler(mcFrame);
-    }
+    // for (U8 mcId = 0; mcId < 8; mcId++) {
+    //     std::array<Fw::Buffer, 8> vcFrames;
+    //     for (U8 vcId = 0; vcId < 8; vcId++) {
+    //         Fw::Buffer sdu;
+    //         m_virtualChannels[mcId][vcId].propogate(sdu);
+    //         vcFrames[vcId] = sdu;
+    //     }
+    //     // Multiplex VC frames into Master Channel
+    //     Fw::Buffer mcFrame;
+    //     m_masterChannels[mcId].VirtualChannelMultiplexing_handler(vcFrames);
+    //     m_masterChannels[mcId].MasterChannelGeneration_handler(mcFrame);
+    // }
 
     // Generate final Physical Channel frames
     Fw::Buffer finalFrame;
@@ -69,10 +85,11 @@ void TMSpaceDataLinkProtocol::frame(const U8* const data, const U32 size, Fw::Co
 
         Fw::SerializeBufferBase& serializer = buffer.getSerializeRepr();
         Fw::SerializeStatus status;
-        status = m_transferFrame.serialize(serializer, m_transferData, data, size);
-        FW_ASSERT(status == Fw::FW_SERIALIZE_OK, status);
+        // status = m_transferFrame.serialize(serializer, m_transferData, data, size);
+        // FW_ASSERT(status == Fw::FW_SERIALIZE_OK, status);
 
-        buffer.setSize(TM_TRANSFER_FRAME_SIZE(m_dataFieldSize));
+        // buffer.setSize(TM_TRANSFER_FRAME_SIZE(m_dataFieldSize));
+
         m_interface->send(buffer);
 
         // Update frame counts
--- Svc/FramingProtocol/CCSDSProtocols/TMSpaceDataLink/Services.cpp
+++ Svc/FramingProtocol/CCSDSProtocols/TMSpaceDataLink/Services.cpp
@@ -1,168 +1,37 @@
-// VirtualChannelAccess.cpp
 #include "Services.hpp"
+#include "Fw/Com/ComBuffer.hpp"
 #include "Fw/Types/String.hpp"
+#include "Os/Queue.hpp"
 
 namespace TMSpaceDataLink {
+template<typename SDU_t,
+         typename SAP_t,
+         typename ServiceParams_t,
+         typename ServiceTransferPrimitive_t,
+         SERVICE_TRANSFER_TYPE_t SERVICE_TRANSFER_TYPE>
+TMService<SDU_t, SAP_t, ServiceParams_t, ServiceTransferPrimitive_t, SERVICE_TRANSFER_TYPE>::
+    TMService(ServiceParams_t const &serviceParams, FwSizeType const qDepth) :
+    sap(serviceParams.sap), m_serviceParams(serviceParams)
+{
+    Os::Queue::Status status;
+    m_serviceParams.primQ.create(serviceName, qDepth, sizeof(ServiceTransferPrimitive_t));
+    FW_ASSERT(status == Os::Queue::Status::OP_OK, status);
+}
 
-VirtualChannelAccess::VirtualChannelAccess(U32 maxQueueDepth)
-    : m_maxQueueDepth(maxQueueDepth) {}
-
-bool VirtualChannelAccess::configureChannel(const VcConfig& config) {
-    std::lock_guard<std::mutex> lock(m_mutex);
-
-    // Validate VCID
-    if (config.virtualChannelId >= MAX_VIRTUAL_CHANNELS) {
-        return false;
-    }
-
-    // Create or update channel configuration
-    m_channels[config.virtualChannelId].config = config;
+// NOTE this is fine for sync services.
+// For async we would want something running its own thread to call this constantly
+// and for periodic it would have strict timing relationships
+template<typename SDU_t,
+         typename SAP_t,
+         typename ServiceParams_t,
+         typename ServiceTransferPrimitive_t,
+         SERVICE_TRANSFER_TYPE_t SERVICE_TRANSFER_TYPE>
+bool TMService<SDU_t, SAP_t, ServiceParams_t, ServiceTransferPrimitive_t, SERVICE_TRANSFER_TYPE>::
+    transfer(Fw::ComBuffer const &transferBuffer)
+{
+    ServiceTransferPrimitive_t primitive;
+    m_serviceParams.primQ.send(transferBuffer);
     return true;
-}
-
-bool VirtualChannelAccess::request(U8 vcId, const VcaSdu& sdu) {
-    std::lock_guard<std::mutex> lock(m_mutex);
-
-    auto it = m_channels.find(vcId);
-    if (it == m_channels.end()) {
-        return false;  // Channel not configured
-    }
-
-    VirtualChannel& channel = it->second;
-
-    // Validate SDU size
-    if (sdu.size != channel.config.sduSize) {
-        return false;
-    }
-
-    // Check queue depth
-    if (channel.sduQueue.size() >= m_maxQueueDepth) {
-        return false;
-    }
-
-    // Copy SDU data and enqueue
-    VcaSdu newSdu;
-    newSdu.data = new U8[sdu.size];
-    newSdu.size = sdu.size;
-    std::copy(sdu.data, sdu.data + sdu.size, newSdu.data);
-    newSdu.status = sdu.status;
-
-    channel.sduQueue.push(newSdu);
-    channel.frameCount++;
-
-    return true;
-}
-
-bool VirtualChannelAccess::getNextSdu(U8& vcId, VcaSdu& sdu) {
-    std::lock_guard<std::mutex> lock(m_mutex);
-
-    // Find highest priority channel with data
-    auto it = std::min_element(
-        m_channels.begin(),
-        m_channels.end(),
-        [](const auto& a, const auto& b) {
-            if (a.second.sduQueue.empty()) return false;
-            if (b.second.sduQueue.empty()) return true;
-            return a.second.config.priority < b.second.config.priority;
-        }
-    );
-
-    if (it == m_channels.end() || it->second.sduQueue.empty()) {
-        return false;
-    }
-
-    // Get SDU from selected channel
-    vcId = it->first;
-    sdu = it->second.sduQueue.front();
-    it->second.sduQueue.pop();
-
-    return true;
-}
-
-bool VirtualChannelAccess::configureChannel(const VcConfig& config) {
-    if (config.virtualChannelId >= MAX_VIRTUAL_CHANNELS) {
-        return false;
-    }
-
-    VirtualChannel_t& channel = m_channels[config.virtualChannelId];
-
-    // Create OS queue for this channel
-    Fw::String queueName;
-    queueName.format("VC%d", config.virtualChannelId);
-
-    Os::Queue::Status status = channel.queue.create(
-        queueName,
-        10,  // depth
-        MAX_SDU_SIZE
-    );
-
-    if (status != Os::Queue::Status::OP_OK) {
-        return false;
-    }
-
-    channel.isValid = true;
-    channel.config = config;
-    return true;
-}
-
-bool VirtualChannelAccess::request(U8 vcId, const U8* data, U32 size) {
-    if (vcId >= MAX_VIRTUAL_CHANNELS ||
-        !m_channels[vcId].isValid ||
-        size > MAX_SDU_SIZE) {
-        return false;
-    }
-
-    VirtualChannel_t& channel = m_channels[vcId];
-
-    // Send to OS queue
-    Os::Queue::Status status = channel.queue.send(
-        data,
-        size,
-        0,  // priority
-        Os::Queue::BlockingType::NONBLOCKING
-    );
-
-    return (status == Os::Queue::Status::OP_OK);
-}
-
-bool VirtualChannelAccess::getNextSdu(U8& vcId, U8* data, U32& size) {
-    // Check each channel in priority order
-    for (U8 id = 0; id < MAX_VIRTUAL_CHANNELS; id++) {
-        VirtualChannel_t& channel = m_channels[id];
-        if (!channel.isValid) {
-            continue;
-        }
-
-        FwSizeType actualSize;
-        PlatformIntType priority;
-
-        Os::Queue::Status status = channel.queue.receive(
-            data,
-            MAX_SDU_SIZE,
-            Os::Queue::BlockingType::NONBLOCKING
-            actualSize,
-            priority
-        );
-
-        if (status == Os::Queue::Status::OP_OK) {
-            vcId = id;
-            size = actualSize;
-            return true;
-        }
-    }
-    return false;
-}
-
-bool VirtualChannelAccess::hasData() const {
-    std::lock_guard<std::mutex> lock(m_mutex);
-
-    return std::any_of(
-        m_channels.begin(),
-        m_channels.end(),
-        [](const auto& channel) {
-            return !channel.second.sduQueue.empty();
-        }
-    );
 }
 
 } // namespace TMSpaceDataLink
