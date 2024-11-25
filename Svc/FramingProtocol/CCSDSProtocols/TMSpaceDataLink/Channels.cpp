386ab875ac2ff60bc3bf0b09dbd8d220c5e01de7
Reg Marr <reginald.marr@reactiondynamics.space>
2024-11-25 18:49:45 -0500

    WIP support for physical chan

--- Svc/FramingProtocol/CCSDSProtocols/TMSpaceDataLink/Channels.cpp
+++ Svc/FramingProtocol/CCSDSProtocols/TMSpaceDataLink/Channels.cpp
@@ -31,40 +31,40 @@ bool VCAFramedChannel::transfer(Fw::ComBuffer& transferBuffer) {
     return qStatus == Os::Queue::Status::OP_OK;
 }
 
-// template class VirtualChannel<VCAService, TransferFrame>;
-// NOTE could apply templating here to support VCA and VCP both in request and return types
-// template <>
-// bool VCAFramedChannel::ChannelGeneration_handler<VCA_Request_t, TransferFrame>(VCA_Request_t const& request,
-// template <>
-// bool VirtualChannel<VCAService, TransferFrame>::ChannelGeneration_handler(VCA_Request_t const& request,
-//                                                                           TransferFrame& channelOut) {
-//     // Generate Transfer Frame for the Virtual Channel using the VCA prim as the data field
-//     // and the Frame service to create the transfer frame itself
-//     bool status = true;
-//     // Was something like this originally
-//     // FrameSDU_t framePrim;
-//     // status = sender->generateFramePrimitive(request, framePrim);
-//     DataField dataField(request.sdu);
-//     // TODO the ManagedParameters should propogate to this
-//     MissionPhaseParameters_t missionParams;
-//     TransferData_t transferData;
-//     // TODO do this
-//     // transferData.dataFieldDesc = request.statusFields
-//     // NOTE could just as easily get this from the id but this matches the spec better
-//     transferData.virtualChannelId = request.sap.VCID;
-//     // Explicitly do not set this
-//     // transferData.masterChannelFrameCount
-//     // TODO this should some how be propogated from the transfer call or the masterChannel parent
-//     static U8 vcFrameCount = 0;
-//     transferData.virtualChannelFrameCount = vcFrameCount++;
-//     PrimaryHeader primaryHeader(missionParams, transferData);
-//     TransferFrame frame(primaryHeader, dataField);
-//     // FIXME this I think is bad memory management
-//     channelOut = frame;
+template class VirtualChannel<VCAService, TransferFrame>;
+NOTE could apply templating here to support VCA and VCP both in request and return types
+template <>
+bool VCAFramedChannel::ChannelGeneration_handler<VCA_Request_t, TransferFrame>(VCA_Request_t const& request,
+template <>
+bool VirtualChannel<VCAService, TransferFrame>::ChannelGeneration_handler(VCA_Request_t const& request,
+                                                                          TransferFrame& channelOut) {
+    // Generate Transfer Frame for the Virtual Channel using the VCA prim as the data field
+    // and the Frame service to create the transfer frame itself
+    bool status = true;
+    // Was something like this originally
+    // FrameSDU_t framePrim;
+    // status = sender->generateFramePrimitive(request, framePrim);
+    DataField dataField(request.sdu);
+    // TODO the ManagedParameters should propogate to this
+    MissionPhaseParameters_t missionParams;
+    TransferData_t transferData;
+    // TODO do this
+    // transferData.dataFieldDesc = request.statusFields
+    // NOTE could just as easily get this from the id but this matches the spec better
+    transferData.virtualChannelId = request.sap.VCID;
+    // Explicitly do not set this
+    // transferData.masterChannelFrameCount
+    // TODO this should some how be propogated from the transfer call or the masterChannel parent
+    static U8 vcFrameCount = 0;
+    transferData.virtualChannelFrameCount = vcFrameCount++;
+    PrimaryHeader primaryHeader(missionParams, transferData);
+    TransferFrame frame(primaryHeader, dataField);
+    // FIXME this I think is bad memory management
+    channelOut = frame;
 
-//     FW_ASSERT(status, status);
-//     return true;
-// }
+    FW_ASSERT(status, status);
+    return true;
+}
 
 // bool MasterChannelSender::VirtualChannelMultiplexing_handler(const std::array<Fw::Buffer, 8>& vcFrames) {
 //     // Multiplex Virtual Channel frames into Master Channel
