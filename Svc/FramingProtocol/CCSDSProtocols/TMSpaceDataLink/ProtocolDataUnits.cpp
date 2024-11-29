f9b36f3e821ba508521eb4753d4ac5beb60ffbec
Reg Marr <reginald.marr@reactiondynamics.space>
2024-11-29 15:40:20 -0500

    Using pdu base for Primary and Secondary headers

--- Svc/FramingProtocol/CCSDSProtocols/TMSpaceDataLink/ProtocolDataUnits.cpp
+++ Svc/FramingProtocol/CCSDSProtocols/TMSpaceDataLink/ProtocolDataUnits.cpp
@@ -1,4 +1,5 @@
 #include "ProtocolDataUnits.hpp"
+#include <cstddef>
 #include <cstring>
 
 namespace TMSpaceDataLink {
@@ -104,28 +105,35 @@ void ProtocolDataUnit<FieldSize, std::array<U8, FieldSize>>::get(FieldValue_t& v
     (void)std::memcpy(val.data(), this->m_value.data(), val.size());
 }
 
-PrimaryHeader::PrimaryHeader(MissionPhaseParameters_t const& params, TransferData_t& transferData) {
-    // NOTE This order is important since the mission params affect how we interpret the transferData
-    setControlInfo(params);
-    setControlInfo(transferData);
+// nullptr specialization implementations
+Fw::SerializeStatus ProtocolDataUnit<0, std::nullptr_t>::serializeValue(Fw::SerializeBufferBase& buffer) const {
+    FW_ASSERT(0);
+    return Fw::SerializeStatus::FW_SERIALIZE_FORMAT_ERROR;
 }
 
-PrimaryHeader::PrimaryHeader(MissionPhaseParameters_t const& params) {
-    setControlInfo(params);
+Fw::SerializeStatus ProtocolDataUnit<0, std::nullptr_t>::deserializeValue(Fw::SerializeBufferBase& buffer) {
+    FW_ASSERT(0);
+    return Fw::SerializeStatus::FW_SERIALIZE_FORMAT_ERROR;
 }
 
-Fw::SerializeStatus PrimaryHeader::deserialize(Fw::SerializeBufferBase& buffer) {
-    return Fw::SerializeStatus::FW_SERIALIZE_OK;
+void ProtocolDataUnit<0, std::nullptr_t>::set(FieldValue_t const& buffer) {
+    FW_ASSERT(0);
 }
 
-Fw::SerializeStatus PrimaryHeader::serialize(Fw::SerializeBufferBase& buffer) const {
+void ProtocolDataUnit<0, std::nullptr_t>::get(FieldValue_t& buffer) const {
+    FW_ASSERT(0);
+}
+
+// PrimaryHeader Control info specialization implementations
+Fw::SerializeStatus ProtocolDataUnit<PRIMARY_HEADER_SERIALIZED_SIZE, PrimaryHeaderControlInfo_t>::serializeValue(
+    Fw::SerializeBufferBase& buffer) const {
     Fw::SerializeStatus status;
     // Fw::ExternalSerializeBuffer buffer(buffer.getBuffAddr(), buffer.getBuffCapacity());
     U16 firstTwoOctets = 0;
-    firstTwoOctets |= (m_ci.transferFrameVersion & 0x03) << 14;
-    firstTwoOctets |= (m_ci.spacecraftId & 0x3FF) << 4;
-    firstTwoOctets |= (m_ci.virtualChannelId & 0x07) << 1;   // Virtual Channel ID (3 bits)
-    firstTwoOctets |= (m_ci.operationalControlFlag & 0x01);  // Operational Control Field Flag (1 bit)
+    firstTwoOctets |= (m_value.transferFrameVersion & 0x03) << 14;
+    firstTwoOctets |= (m_value.spacecraftId & 0x3FF) << 4;
+    firstTwoOctets |= (m_value.virtualChannelId & 0x07) << 1;   // Virtual Channel ID (3 bits)
+    firstTwoOctets |= (m_value.operationalControlFlag & 0x01);  // Operational Control Field Flag (1 bit)
     status = buffer.serialize(firstTwoOctets);
     FW_ASSERT(status == Fw::FW_SERIALIZE_OK, status);
 
@@ -134,17 +142,17 @@ Fw::SerializeStatus PrimaryHeader::serialize(Fw::SerializeBufferBase& buffer) co
 
     // Bit 32: Transfer Frame Secondary Header Flag (0 if no secondary header)
     // Must be static throughout Mission Phase per 4.1.2.7.2.3
-    dataFieldStatus |= (m_ci.dataFieldStatus.hasSecondaryHeader ? 1 : 0) << 15;
+    dataFieldStatus |= (m_value.dataFieldStatus.hasSecondaryHeader ? 1 : 0) << 15;
 
     // Bit 33: Synchronization Flag
     // 0 for octet-synchronized and forward-ordered Packets or Idle Data
     // 1 for VCA_SDU
     // Must be static within Virtual Channel per 4.1.2.7.3.3
-    dataFieldStatus |= (m_ci.dataFieldStatus.isSyncFlagEnabled ? 1 : 0) << 14;
+    dataFieldStatus |= (m_value.dataFieldStatus.isSyncFlagEnabled ? 1 : 0) << 14;
 
     // Bit 34: Packet Order Flag
     // Set to 0 when Synchronization Flag is 0 per 4.1.2.7.4
-    dataFieldStatus |= m_ci.dataFieldStatus.isPacketOrdered << 13;
+    dataFieldStatus |= m_value.dataFieldStatus.isPacketOrdered << 13;
 
     // Bits 35-36: Segment Length Identifier
     // Must be set to '11' (3) when Synchronization Flag is 0 per 4.1.2.7.5.2
@@ -155,7 +163,7 @@ Fw::SerializeStatus PrimaryHeader::serialize(Fw::SerializeBufferBase& buffer) co
     // Therefore setting to 0 per 4.1.2.7.6.3
     // Set to 0x7FF (11111111111) if no packet starts in frame per 4.1.2.7.6.4
     // Set to 0x7FE (11111111110) if only idle data per 4.1.2.7.6.5
-    dataFieldStatus |= m_ci.dataFieldStatus.firstHeaderPointer;  // Assuming packet starts at beginning
+    dataFieldStatus |= m_value.dataFieldStatus.firstHeaderPointer;  // Assuming packet starts at beginning
     status = buffer.serialize(dataFieldStatus);
     FW_ASSERT(status == Fw::FW_SERIALIZE_OK, status);
 
@@ -166,42 +174,56 @@ Fw::SerializeStatus PrimaryHeader::serialize(Fw::SerializeBufferBase& buffer) co
     return Fw::SerializeStatus::FW_SERIALIZE_OK;
 }
 
+Fw::SerializeStatus ProtocolDataUnit<PRIMARY_HEADER_SERIALIZED_SIZE, PrimaryHeaderControlInfo_t>::deserializeValue(
+    Fw::SerializeBufferBase& buffer) {
+    return Fw::SerializeStatus::FW_SERIALIZE_OK;
+}
+
+PrimaryHeader::PrimaryHeader(MissionPhaseParameters_t const& params, TransferData_t& transferData) {
+    // NOTE This order is important since the mission params affect how we interpret the transferData
+    setControlInfo(params);
+    setControlInfo(transferData);
+}
+
+PrimaryHeader::PrimaryHeader(MissionPhaseParameters_t const& params) {
+    setControlInfo(params);
+}
+
 void PrimaryHeader::setControlInfo(MissionPhaseParameters_t const& params) {
     // These fields are mission parameters and must be constant over the course of the mission phase
-    m_ci.transferFrameVersion = params.transferFrameVersion;
+    this->m_value.transferFrameVersion = params.transferFrameVersion;
     // as per CCSDS 132.0-B-3 4.1.2.1
     // the Spacecraft Id is placed with a 4 bit offset within the first octet
-    m_ci.spacecraftId = params.spaceCraftId;
-    m_ci.operationalControlFlag = params.hasOperationalControlFlag;
-    m_ci.dataFieldStatus.hasSecondaryHeader = params.hasSecondaryHeader;
-    m_ci.dataFieldStatus.isSyncFlagEnabled = params.isSyncFlagEnabled;
+    this->m_value.spacecraftId = params.spaceCraftId;
+    this->m_value.operationalControlFlag = params.hasOperationalControlFlag;
+    this->m_value.dataFieldStatus.hasSecondaryHeader = params.hasSecondaryHeader;
+    this->m_value.dataFieldStatus.isSyncFlagEnabled = params.isSyncFlagEnabled;
 
     // Recommended value as per CCSDS 132.0-B-3 4.1.2.7.4
-    m_ci.dataFieldStatus.isPacketOrdered = false;
+    this->m_value.dataFieldStatus.isPacketOrdered = false;
 
     // If the sync flag field is true then segementLengthId is undefined
     // otherwise set it to 0b11
-    if (!m_ci.dataFieldStatus.isSyncFlagEnabled) {
-        m_ci.dataFieldStatus.segmentLengthId = 0b11;
+    if (!this->m_value.dataFieldStatus.isSyncFlagEnabled) {
+        this->m_value.dataFieldStatus.segmentLengthId = 0b11;
     }
 }
 
 void PrimaryHeader::setControlInfo(TransferData_t& transferData) {
     // Set the transfer specific control info
-    m_ci.virtualChannelId = transferData.virtualChannelId;
-    m_ci.masterChannelFrameCount = transferData.masterChannelFrameCount;
-    m_ci.virtualChannelFrameCount = transferData.virtualChannelFrameCount;
-
-    m_ci.dataFieldStatus.firstHeaderPointer = transferData.dataFieldDesc.packetOffset;
+    this->m_value.virtualChannelId = transferData.virtualChannelId;
+    this->m_value.masterChannelFrameCount = transferData.masterChannelFrameCount;
+    this->m_value.virtualChannelFrameCount = transferData.virtualChannelFrameCount;
+    this->m_value.dataFieldStatus.firstHeaderPointer = transferData.dataFieldDesc.packetOffset;
 
     // The first header pointer is only defined if the sync flag is not enabled
     // We don't currently support anything other than VCAS
-    if (!m_ci.dataFieldStatus.isSyncFlagEnabled) {
+    if (!this->m_value.dataFieldStatus.isSyncFlagEnabled) {
         if (transferData.dataFieldDesc.isFieldDataExtendedPacket) {
-            FW_ASSERT(m_ci.dataFieldStatus.firstHeaderPointer == EXTEND_PACKET_TYPE);
+            FW_ASSERT(this->m_value.dataFieldStatus.firstHeaderPointer == EXTEND_PACKET_TYPE);
         }
         if (transferData.dataFieldDesc.isOnlyIdleData) {
-            FW_ASSERT(m_ci.dataFieldStatus.firstHeaderPointer == IDLE_TYPE);
+            FW_ASSERT(this->m_value.dataFieldStatus.firstHeaderPointer == IDLE_TYPE);
         }
     }
 }
@@ -248,13 +270,17 @@ bool FrameErrorControlField::insert(U8* startPtr, Fw::SerializeBufferBase& buffe
 // NOTE this should probably be done on the instatiation side of things
 // Instantiate the base class
 template class ProtocolDataUnitBase<247, std::array<U8, 247>>;
-    template class ProtocolDataUnitBase<sizeof(U16), U16>;
+template class ProtocolDataUnitBase<sizeof(U16), U16>;
+template class ProtocolDataUnitBase<6, PrimaryHeaderControlInfo_t>;
+template class ProtocolDataUnitBase<0, std::nullptr_t>;
 
 // Instantiate the primary template (if needed)
 // FIXME I get linter errors here but they don't seem to affect compilation:
 // In template: dependent using declaration resolved to type without 'typename' (lsp)
 template class ProtocolDataUnit<247, std::array<U8, 247>>;
 template class ProtocolDataUnit<sizeof(U16), U16>;
+template class ProtocolDataUnit<6, PrimaryHeaderControlInfo_t>;
+template class ProtocolDataUnit<0, std::nullptr_t>;
 
 // Instantiate DataField
 template class DataField<247>;
